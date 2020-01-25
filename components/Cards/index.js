c6ce7abe8f3b91311365fa01ecd2a91bb2882f40
Devin Cooper <Dcbekillin@Devins-MacBook-Pro.local>
2020-01-25 13:26:29 -0800

    MVP-one

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,4 +1,4 @@
-// STEP 3: Create Article cards.
+// 4STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
 // Stduy the response data you get back, closely.
@@ -17,3 +17,63 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.card-container');
+
+//const axiosPromise =  axios.get('https://lambda-times-backend.herokuapp.com/articles')
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    response.data.articles.javascript.forEach(item => {
+        const newCard = createArticle(item);
+    });
+    .then(response => {
+        response.data.articles.javascript.forEach(newCard =>{
+            cardsContainer.appendChild(newCard(data.articles.javascript.headline, data.articles.javascript.authorName, data.articles.javascript.authorPhoto))
+        });
+    });
+});
+
+    
+
+
+
+
+
+
+
+
+
+const createArticle = (headline, authorName, authorPhoto ) =>{
+    const card = document.createElement('div')
+    const headlines = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div')
+    const imgAuthorPhoto = document.createElement('img')
+    const spanAuthorName = document.createElement('span')
+
+
+    card.appendChild(headlines);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(imgAuthorPhoto);
+    author.appendChild(spanAuthorName);
+
+card.classList.add('card')
+headlines.classList.add('headline')
+author.classList.add('author')
+imgContainer.classList.add('img-container')
+spanAuthorName.classList.add('span')
+
+
+headlines.textContent = headline;
+imgAuthorPhoto.src = authorPhoto;
+spanAuthorName.textContent = authorName;
+
+return card;
+
+
+
+
+}
+
